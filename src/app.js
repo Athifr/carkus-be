6b43f7b69f8df74db48a6dc1895cafe924f8089b
Athif Tafrihan Achyar <athifr56@gmail.com>
2026-01-17 23:47:19 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ import { users } from "./routes/users.js";
 
 export const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use("/auth", auth);
